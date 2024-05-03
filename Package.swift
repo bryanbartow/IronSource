ac98c3adf6356ccd5567f0f953d92d6da17af7f0
Bryan Bartow <bryan@bartow.family>
2024-05-03 10:40:50 -0500

    Will this work?

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "IronSource",
+    name: "IronSourcePackage",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "IronSource",
-            targets: ["IronSource"]),
+            name: "IronSourcePackage",
+            targets: ["IronSourceBinary"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "IronSource"),
+        /*.target(
+            name: "IronSource"),*/
         .binaryTarget(
             name: "IronSourceBinary",
             url: "https://raw.githubusercontent.com/bryanbartow/IronSourceBinary/main/IronSource.xcframework.zip",
             checksum: "77118dabaae0c1dac0eb0b380e4e4ba015e1b1d43f14212c082cc3e4adbaeec3"
         ),
-        .testTarget(
+        /*.testTarget(
             name: "IronSourceTests",
-            dependencies: ["IronSource"]),
+            dependencies: ["IronSource"]),*/
     ]
 )
