1b534859514dc443cda5b015b1002e85486e670c
Bryan Bartow <bryan@bartow.family>
2024-05-03 09:17:29 -0500

    Re-add the binary

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "IronSource"),
-        /*.binaryTarget(
+        .binaryTarget(
             name: "IronSourceBinary",
             url: "https://raw.githubusercontent.com/bryanbartow/IronSourceBinary/main/IronSource.xcframework.zip",
             checksum: "77118dabaae0c1dac0eb0b380e4e4ba015e1b1d43f14212c082cc3e4adbaeec3"
-        ),*/
+        ),
         .testTarget(
             name: "IronSourceTests",
             dependencies: ["IronSource"]),
